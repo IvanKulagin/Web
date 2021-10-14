86ac25fad287aa2f8740e6dc2215393dc46a7ad6
IvanKulagin <19168617+IvanKulagin@users.noreply.github.com>
2021-10-14 15:20:11 +0300

    Add files via upload

---
+++ Lab1/handler.php
@@ -0,0 +1,10 @@
+<table class="result_table">
+    <tr>
+        <th>X</th>
+        <th>Y</th>
+        <th>R</th>
+        <th>Result</th>
+        <th>Submit time</th>
+        <th>Calc time</th>
+    </tr>
+    </table>
